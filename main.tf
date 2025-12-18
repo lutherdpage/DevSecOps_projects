03b84b13121624e111f9d0b0b96babe4005e2b99
Luther Page <lutherdpage@gmail.com>
2025-12-18 00:06:02 -0600

    Clean repo and remove Terraform provider files

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "bucket_one" {
-  bucket = "bucket_one"
+resource "aws_s3_bucket" "bucketluthertest" {
+  bucket = "bucketluthertest"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "bucketluthertest"
+    region         = "eu-west-2"
+    key           = "CHANGE/terraform.tfstate"
+  }
+}
+resource "aws_instance" "test6" {
+  ami           = "ami-099400d52583dd8c4" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
+  instance_type = "t2.micro"  # Change to the desired instance type
+  tags = {
+    Name = "instance_luthertest"
+}
 }
